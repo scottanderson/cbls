20b8e431c38ccf5dee0ea6c1b8fb69f842f25ca9
jeffrey.shorf <jeffrey.shorf@57ef982a-a093-11de-89ee-f3da9a46d971>
2009-09-14 04:31:14 +0000

    Start of the PacketBuffer.

---
+++ CBLS/src/util/PacketBuffer.h
@@ -0,0 +1,15 @@
+/*
+ * PacketBuffer.h
+ *
+ *  Created on: Sep 13, 2009
+ *      Author: Jeffrey Shorf (jeffrey.shorf@gmail.com)
+ */
+
+#ifndef PACKETBUFFER_H_
+#define PACKETBUFFER_H_
+
+//Get Packets
+
+//Read Packets
+
+#endif /* PACKETBUFFER_H_ */
