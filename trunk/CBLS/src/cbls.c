253f30d8cd1f9684537c22a2b5c56448b4e4a2e0
scotta <scotta@57ef982a-a093-11de-89ee-f3da9a46d971>
2009-09-18 08:45:00 +0000

    first pass of connection code

--- trunk/CBLS/src/cbls.c
+++ trunk/CBLS/src/cbls.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <errno.h>
+#include <time.h>
 #if defined(__WIN32__)
 #  include <winsock2.h>
 #else
@@ -9,13 +12,249 @@
 #include "cbls.h"
 
 
+int cbls_open_max = 0;
+struct cbls_file *cbls_files = 0;
+
+int nr_open_files = 3;
+
 int high_fd = 0;
 fd_set cbls_rfds, cbls_wfds;
 struct timeval server_start_time;
 
+static void loopZ (void) __attribute__((__noreturn__));
+
+struct timeval loopZ_timeval;
+
 char *cbls_version = "0.1";
 
-int main(int argc __attribute__((__unused__)), char **argv __attribute__((__unused__)), char **envp)
+
+void
+cbls_log (const char *fmt, ...)
+{
+	va_list ap;
+	char buf[2048];
+	int len;
+	time_t t;
+	struct tm tm;
+
+	time(&t);
+	localtime_r(&t, &tm);
+	strftime(buf, 21, "%H:%M:%S %m/%d/%Y\t", &tm);
+	va_start(ap, fmt);
+	len = vsnprintf(&buf[20], sizeof(buf) - 24, fmt, ap);
+	va_end(ap);
+	if (len == -1)
+		len = sizeof(buf) - 24;
+	len += 20;
+	buf[len++] = '\n';
+	write(/*log_fd*/1, buf, len);
+	//SYS_fsync(log_fd);
+}
+
+int get_open_max(void) {
+	int om;
+
+#if defined(_SC_OPEN_MAX)
+	om = sysconf(_SC_OPEN_MAX);
+#elif defined(RLIMIT_NOFILE)
+	{
+		struct rlimit rlimit;
+
+		if (getrlimit(RLIMIT_NOFILE, &rlimit)) {
+			hxd_log("main: getrlimit: %s", strerror(errno));
+			exit(1);
+		}
+		om = rlimit.rlim_max;
+	}
+#elif defined(HAVE_GETDTABLESIZE)
+	om = getdtablesize();
+#elif defined(OPEN_MAX)
+	om = OPEN_MAX;
+#else
+	om = sizeof(fd_set)*8;
+#endif
+
+	if (om > (int)(FD_SETSIZE*sizeof(int)*8))
+		om = (int)(FD_SETSIZE*sizeof(int)*8);
+
+#if defined(__WIN32__)
+	om = 4096;
+#endif
+
+	return om;
+}
+
+
+
+void cbls_fd_add (int fd)
+{
+	if (high_fd < fd)
+		high_fd = fd;
+}
+
+void cbls_fd_del (int fd)
+{
+	if (high_fd == fd) {
+		for (fd--; fd && !FD_ISSET(fd, &cbls_rfds); fd--)
+			;
+		high_fd = fd;
+	}
+}
+
+void cbls_fd_set (int fd, int rw)
+{
+	if (rw & FDR)
+		FD_SET(fd, &cbls_rfds);
+	if (rw & FDW)
+		FD_SET(fd, &cbls_wfds);
+}
+
+void cbls_fd_clr (int fd, int rw)
+{
+	if (rw & FDR)
+		FD_CLR(fd, &cbls_rfds);
+	if (rw & FDW)
+		FD_CLR(fd, &cbls_wfds);
+}
+
+static void loopZ (void) {
+	fd_set rfds, wfds;
+	struct timeval before, tv;
+
+	gettimeofday(&tv, 0);
+	for (;;) {
+		register int n, i;
+
+		/*if (timer_list) {
+			gettimeofday(&before, 0);
+			timer_check(&tv, &before);
+			if (timer_list)
+				tv = timer_list->tv;
+		}*/
+		rfds = cbls_rfds;
+		wfds = cbls_wfds;
+		n = select(high_fd + 1, &rfds, &wfds, 0, /*timer_list ? &tv :*/ 0);
+		if (n < 0) {
+			if (errno != EINTR) {
+				cbls_log("loopZ: select: %s", strerror(errno));
+				exit(1);
+			}
+		}
+		gettimeofday(&tv, 0);
+		loopZ_timeval = tv;
+		/*if (timer_list) {
+			timer_check(&before, &tv);
+		}*/
+		if (n <= 0)
+			continue;
+		for (i = 0; i < high_fd + 1; i++) {
+			if (FD_ISSET(i, &rfds) && FD_ISSET(i, &cbls_rfds)) {
+				if (cbls_files[i].ready_read)
+					cbls_files[i].ready_read(i);
+				n--;
+				if (!n)
+					break;
+			}
+			if (FD_ISSET(i, &wfds) && FD_ISSET(i, &cbls_wfds)) {
+				if (cbls_files[i].ready_write)
+					cbls_files[i].ready_write(i);
+				n--;
+				if (!n)
+					break;
+			}
+		}
+	}
+}
+
+
+int
+inet_ntoa_r (struct in_addr in, char *buf, size_t buflen)
+{
+	u_int32_t addr = in.s_addr;
+	register u_int8_t *addr_p = (u_int8_t *)&addr, *t;
+	register unsigned int i, pos;
+	u_int8_t tmp[4];
+
+	for (i = 4, pos = 0; ; addr_p++) {
+		i--;
+		t = tmp;
+		do {
+			*t++ = "0123456789"[*addr_p % 10];
+		} while (*addr_p /= 10);
+		for (; t > tmp; pos++) {
+			if (pos >= buflen)
+				return -1;
+			buf[pos] = *--t;
+		}
+		if (!i)
+			break;
+		if (pos >= buflen)
+			return -1;
+		buf[pos++] = '.';
+	}
+
+	if (pos >= buflen)
+		return -1;
+	buf[pos] = 0;
+
+	return pos;
+}
+
+void
+inaddr2str (char abuf[HOSTLEN+1], struct SOCKADDR_IN *sa)
+{
+#ifdef CONFIG_IPV6
+	inet_ntop(AFINET, (char *)&sa->SIN_ADDR, abuf, HOSTLEN+1);
+#else
+	inet_ntoa_r(sa->SIN_ADDR, abuf, 16);
+#endif
+}
+
+static void
+listen_ready_read (int fd)
+{
+	int s;
+	struct sockaddr_in saddr;
+	int siz = sizeof(saddr);
+	char abuf[16];
+//	struct cbls_conn *cbls;
+
+	s = accept(fd, (struct sockaddr *)&saddr, &siz);
+	if (s < 0) {
+		cbls_log("cbls: accept: %s", strerror(errno));
+		return;
+	}
+	nr_open_files++;
+	inaddr2str(abuf, &saddr);
+	if (nr_open_files >= cbls_open_max) {
+		cbls_log("%s:%u: %d >= cbls_open_max (%d)", abuf, ntohs(saddr.sin_port), s, cbls_open_max);
+//		socket_close(s);
+		nr_open_files--;
+		return;
+	}
+
+//	fd_closeonexec(s, 1);
+//	socket_blocking(s, 0);
+	if (high_fd < s)
+		high_fd = s;
+
+//	cbls = cbls_new();
+//	cbls->fd = s;
+//	cbls->sockaddr = saddr;
+
+	/*
+	 * Make sure known bad addresses are banned before
+	 * they can fill up the connection spam queue.
+	 */
+//	if (check_banlist(cbls))
+//		return;
+	cbls_log("%s:%u -- cbls connection accepted", abuf, ntohs(saddr.sin_port));
+
+//	cbls_accepted(cbls);
+}
+
+int
+main(int argc __attribute__((__unused__)), char **argv __attribute__((__unused__)), char **envp)
 {
 #if defined(__WIN32__)
 	/* init winsock */
@@ -24,12 +263,15 @@ int main(int argc __attribute__((__unused__)), char **argv __attribute__((__unus
 	WSAStartup(1, &wsadata);
 #endif
 
+	cbls_open_max = get_open_max();
+	cbls_files = malloc(cbls_open_max * sizeof(struct cbls_file));
+	memset(cbls_files, 0, cbls_open_max * sizeof(struct cbls_file));
 	FD_ZERO(&cbls_rfds);
 	FD_ZERO(&cbls_wfds);
 
 	int listen_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (listen_sock < 0) {
-		printf("socket() failed");
+		cbls_log("socket() failed");
 		exit(1);
 	}
 
@@ -39,16 +281,25 @@ int main(int argc __attribute__((__unused__)), char **argv __attribute__((__unus
 
 	int r = bind(listen_sock, (struct sockaddr *)&saddr, sizeof(saddr));
 	if(r < 0) {
-		printf("bind() failed");
+		cbls_log("bind() failed");
 		exit(1);
 	}
 
 	if(listen(listen_sock, 5) < 0) {
-		printf("listen() failed");
+		cbls_log("listen() failed");
 		exit(1);
 	}
 
-	printf("cbls version %s started", cbls_version);
+	cbls_files[listen_sock].ready_read = listen_ready_read;
+	cbls_fd_set(listen_sock, FDR);
+	if (high_fd < listen_sock)
+		high_fd = listen_sock;
+//	fd_closeonexec(listen_sock, 1);
+//	socket_blocking(listen_sock, 0);
+
+	cbls_log("cbls version %s started", cbls_version);
+
+	loopZ();
 
 #if defined(__WIN32__)
 	WSACleanup();
--- trunk/CBLS/src/cbls.h
+++ trunk/CBLS/src/cbls.h
@@ -9,6 +9,28 @@ typedef unsigned __int16 u_int16_t;
 typedef unsigned __int32 u_int32_t;
 #endif
 
+/* IPv6 */
+#ifdef CONFIG_IPV6
+#define HOSTLEN 63
+#define SOCKADDR_IN sockaddr_in6
+#define SIN_PORT sin6_port
+#define SIN_FAMILY sin6_family
+#define SIN_ADDR sin6_addr
+#define S_ADDR s6_addr
+#define AFINET AF_INET6
+#define IN_ADDR in6_addr
+#else
+/* IPv4 */
+#define HOSTLEN 15
+#define SOCKADDR_IN sockaddr_in
+#define SIN_PORT sin_port
+#define SIN_FAMILY sin_family
+#define SIN_ADDR sin_addr
+#define S_ADDR s_addr
+#define AFINET AF_INET
+#define IN_ADDR in_addr
+#endif
+
 struct qbuf {
 	u_int32_t pos, len;
 	u_int8_t *buf;
@@ -17,12 +39,8 @@ struct qbuf {
 struct cbls_conn {
 	struct cbls_conn *next, *prev;
 	int fd;
-	int wfd;
-	int identfd;
 	void (*rcv)(struct cbls_conn *);
-	void (*real_rcv)(struct cbls_conn *);
 	struct qbuf in, out;
-	struct qbuf read_in;
 	struct SOCKADDR_IN *sockaddr;
 
 	u_int16_t uid;
@@ -30,5 +48,30 @@ struct cbls_conn {
 	struct timeval login_tv;
 	struct timeval idle_tv;
 };
+
+struct cbls_file {
+	union {
+		void *ptr;
+		struct cbls_conn *cbls;
+	} conn;
+	void (*ready_read)(int fd);
+	void (*ready_write)(int fd);
+};
+
+extern struct cbls_file *cbls_files;
+
+extern int cbls_open_max;
+extern int nr_open_files;
+
+extern int high_fd;
+
+extern fd_set cbls_rfds, cbls_wfds;
+
+extern void cbls_fd_add (int fd);
+extern void cbls_fd_del (int fd);
+extern void cbls_fd_set (int fd, int rw);
+extern void cbls_fd_clr (int fd, int rw);
+#define FDR	1
+#define FDW	2
 
 #endif
